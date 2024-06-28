9709715c050daebef934d11be2e482bb167708c9
RehanShanavas <rehanshanavas@outlook.com>
2024-06-28 09:41:11 -0600

    Wrote the 'sya()' function Changed 'infix_operations()' to 'perform_operations()' since the function name didn't make sense

--- shuntingyardalgorithm.py
+++ shuntingyardalgorithm.py
@@ -1,12 +1,16 @@
+# Declaring the operators in the order of precedence from high to low
+# Reverses the order to make it from low to high (so we can compare the order using indices)
+OPERATORS = [op for op in "^/*+-"][::-1]
+
 def get_input():
   return input("Enter the expression to be evaluated : ")
 
 def separate_tokens(expression):
-''' Takes a mathematical expression of type string and returns a list with all
-    the operations(as type string) and numbers(as type integer) as elements of
-    the list.
-    This function makes iterating over the tokens easier.
-'''
+  ''' Takes a mathematical expression of type string and returns a list with all
+      the operations(as type string) and numbers(as type integer) as elements of
+      the list.
+      This function makes iterating over the tokens easier.
+  '''
   token_list = []
   is_reading_number = False
   number_read = ""
@@ -30,15 +34,35 @@ def separate_tokens(expression):
   return token_list
 
 def sya(list):
-  return
+  queue = []
+  stack = []
+  for token in list:
+    if type(token) == type(9):
+      queue.append(token)
+    elif token in OPERATORS:
+      token_rank = OPERATORS.index(token)
+      while len(stack)>0 and (stack[-1] in OPERATORS) and OPERATORS.index(stack[-1]) > token_rank:
+        queue.append(stack.pop())
+      stack.append(token)
+    elif token == '(':
+      stack.append(token)
+    elif token == ')':
+      while len(stack)>0 and stack[-1]!='(':
+        queue.append(stack.pop())
+      stack.pop()
+  while len(stack)>0:
+    queue.append(stack.pop())
 
-def infix_operations(reverse_polish):
+  print(queue)
+  return queue
+
+def perform_operations(reverse_polish):
   return
 
 def evaluate(expression):
   token_list = separate_tokens(expression)
   reverse_polish_stack = sya(token_list)
-  answer = infix_operations(reverse_polish_stack)
+  answer = perform_operations(reverse_polish_stack)
   return answer
 
 def main():
